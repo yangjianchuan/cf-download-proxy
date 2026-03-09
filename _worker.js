e990ae998bb70940d144142dfb069c4592a63674
jjaw <jjaw@jjaw.cn>
2026-03-10 01:12:39 +0800

    修复内容类型处理，避免将HTML类型返回给浏览器以防止解析错误

--- _worker.js
+++ _worker.js
@@ -105,7 +105,7 @@ export default {
             newHeaders.set("Location",`${url.origin}/${toUrl}`);
 
             // 处理content-type,不返回html类型，否则会被浏览器当成html解析，导致无法下载
-            const contentType = newHeaders.get("content-type");
+            let contentType = newHeaders.get("content-type");
             if(contentType?.includes("text/html")){
                 contentType = contentType.replace("text/html","text/cf-html");
                 newHeaders.set("content-type", contentType);
